5c94b4e9e4205ff2c7e278f8c9ffc34f44f43b08
Harshit2412 <harshit1329.becse24@chitkara.edu.in>
2025-01-26 00:11:01 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%c",&c);
+    int sum=a+b;
+    int diff=a-b;
+    int product=a*b;
+    int division=a/b;
+    if(c=="+"){
+        printf("%d",sum);
+    }
+    else if(c=="-"){
+        printf("%d",diff);
+    }
+    else if(c=="*"){
+        printf("%d",product);
+    }
+    else if(c=="/"){
+        printf{"%d",division};
+    }
+    else{
+        printf("error");
+    }
+   return 0;
+}
